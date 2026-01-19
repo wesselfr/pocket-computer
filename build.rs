b51759f757acc1eb8735e1ee0d8a025bf0901022
Wesselfr <wessel.frijters@gmail.com>
2026-01-19 18:58:08 +0100

    Include GIT_HASH. Optimization level change

--- build.rs
+++ build.rs
@@ -1,7 +1,22 @@
+use std::process::Command;
+
 fn main() {
     linker_be_nice();
     // make sure linkall.x is the last linker script (otherwise might cause problems with flip-link)
     println!("cargo:rustc-link-arg=-Tlinkall.x");
+
+    // Include GIT_HASH
+    let output = Command::new("git")
+        .args(["rev-parse", "--short", "HEAD"])
+        .output()
+        .expect("Failed to run git");
+
+    let git_hash = String::from_utf8(output.stdout).unwrap();
+    let git_hash = git_hash.trim();
+
+    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+    println!("cargo:rerun-if-changed=.git/HEAD");
+    println!("cargo:rerun-if-changed=.git/refs");
 }
 
 fn linker_be_nice() {
